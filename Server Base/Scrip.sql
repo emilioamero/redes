8fbf9203caed14d8a25de3a3ba04e818a2be777c
emilioamero <emilio.amero@gmail.com>
2020-06-26 12:41:19 -0300

    SERVER BASE

---
+++ Server Base/Scrip.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Community Edition- MySQL GUI v8.05 
+MySQL - 5.7.27-log : Database - redes
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`redes` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `redes`;
+
+/*Table structure for table `customers` */
+
+DROP TABLE IF EXISTS `customers`;
+
+CREATE TABLE `customers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `active` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `customers` */
+
+LOCK TABLES `customers` WRITE;
+
+insert  into `customers`(`id`,`email`,`name`,`active`) values (1,'e','e',0),(2,'yami','yami',0);
+
+UNLOCK TABLES;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
---
+++ Server Base/app/models/db.js
@@ -0,0 +1,38 @@
+const mysql = require("mysql");
+
+/* Es incesasario leeer los datos d ela conexion en otro archivo
+const dbConfig = require("../config/db.config.js");
+
+ const connection = mysql.createConnection({
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  database: dbConfig.DB
+}); */
+
+/* CREATE TABLE IF NOT EXISTS `customers` (
+    id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL,
+    NAME VARCHAR(255) NOT NULL,
+    active BOOLEAN DEFAULT FALSE
+  ) ENGINE=INNODB DEFAULT CHARSET=utf8;
+  
+ */
+const connection = mysql.createConnection({
+    host: "192.168.1.25",
+    user: "root",
+    password: "33309073",
+    database: "redes",
+    port: 3306
+
+  });
+
+
+
+// open the MySQL connection
+connection.connect(error => {
+  if (error) throw error;
+  console.log("Base de Datos Conectada");
+});
+
+module.exports = connection;
